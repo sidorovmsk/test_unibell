ff5c764a0fede1e3bf3ce90fe2109b031c4d1c58
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2024-10-14 15:50:16 +0300

    added controller descriptions for Swagger

--- src/main/java/org/example/test_unibell/controllers/ClientController.java
+++ src/main/java/org/example/test_unibell/controllers/ClientController.java
@@ -1,5 +1,9 @@
 package org.example.test_unibell.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.example.test_unibell.models.Client;
 import org.example.test_unibell.services.ClientService;
@@ -20,16 +24,34 @@ import java.util.Optional;
 public class ClientController {
     private final ClientService clientService;
 
+    @Operation(summary = "Добавить нового клиента", description = "Создает нового клиента и сохраняет его в базе данных.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Клиент успешно добавлен"),
+            @ApiResponse(responseCode = "400", description = "Ошибка запроса"),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+    })
     @PostMapping("/add")
     public ResponseEntity<Client> addClient(@RequestBody Client client) {
         return clientService.addClient(client);
     }
 
+    @Operation(summary = "Получить всех клиентов", description = "Возвращает список всех клиентов.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список клиентов успешно получен"),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+    })
     @GetMapping("/all")
     public ResponseEntity<List<Client>> getClients() {
         return clientService.getClients();
     }
 
+    @Operation(summary = "Получить клиента по ID", description = "Возвращает клиента по заданному идентификатору.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Клиент успешно получен"),
+            @ApiResponse(responseCode = "404", description = "Клиент не найден"),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+    })
+    @Parameter(name = "id", description = "ID клиента", required = true)
     @GetMapping("/{id}")
     public Optional<Client> getClientById(@PathVariable Long id) {
         return clientService.getClientById(id);
--- src/main/java/org/example/test_unibell/controllers/ContactController.java
+++ src/main/java/org/example/test_unibell/controllers/ContactController.java
@@ -1,5 +1,10 @@
 package org.example.test_unibell.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.example.test_unibell.dtos.ContactDTO;
 import org.example.test_unibell.models.Contact;
@@ -21,18 +26,42 @@ import java.util.List;
 public class ContactController {
     private final ContactService contactService;
 
+    @Operation(summary = "Добавить новый контакт для клиента", description = "Создает новый контакт для клиента на основе ID клиента, типа контакта и значения.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Контакт успешно добавлен"),
+            @ApiResponse(responseCode = "400", description = "Ошибка запроса"),
+            @ApiResponse(responseCode = "404", description = "Клиент не найден"),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+    })
     @PostMapping("/add")
     public ResponseEntity<Contact> addContact(@RequestBody ContactDTO contactDTO) {
         Contact contact = contactService.addContactForClient(contactDTO.getClientId(), contactDTO.getTypeName(), contactDTO.getContactValue());
         return ResponseEntity.status(HttpStatus.CREATED).body(contact);
     }
 
+    @Operation(summary = "Получить все контакты клиента", description = "Возвращает список всех контактов для заданного клиента по ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список контактов успешно получен"),
+            @ApiResponse(responseCode = "404", description = "Клиент не найден"),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+    })
+    @Parameter(name = "clientId", description = "ID клиента", required = true)
     @GetMapping("/client/{clientId}")
     public ResponseEntity<List<Contact>> getContactsByClientId(@PathVariable Long clientId) {
         List<Contact> contacts = contactService.getContactsByClientId(clientId);
         return ResponseEntity.ok(contacts);
     }
 
+    @Operation(summary = "Получить контакты клиента по типу", description = "Возвращает список контактов для клиента по заданному типу контакта и ID клиента.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список контактов успешно получен"),
+            @ApiResponse(responseCode = "404", description = "Клиент или контакты не найдены"),
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+    })
+    @Parameters({
+            @Parameter(name = "clientId", description = "ID клиента", required = true),
+            @Parameter(name = "typeName", description = "Тип контакта", required = true)
+    })
     @GetMapping("/client/{clientId}/type/{typeName}")
     public ResponseEntity<List<Contact>> getContactsByClientIdAndType(@PathVariable Long clientId, @PathVariable String typeName) {
         List<Contact> contacts = contactService.getContactsByClientIdAndType(clientId, typeName);
