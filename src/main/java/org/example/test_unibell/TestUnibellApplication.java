d96f66442fec587b082a60c0b571ff6eae0d968c
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2024-10-14 10:44:51 +0300

    init

---
+++ src/main/java/org/example/test_unibell/TestUnibellApplication.java
@@ -0,0 +1,13 @@
+package org.example.test_unibell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUnibellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestUnibellApplication.class, args);
+    }
+
+}
